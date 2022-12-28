c106ba5b4cc47459356ce4991b748794360d9f6d
nguyen quang hoang <hoangdev161201@gmail.com>
2022-12-28 14:57:55 +0700

    upgrade

--- app/main/CoffeePrice/page.tsx
+++ app/main/CoffeePrice/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client'
+import {useEffect} from 'react'
+import {useAppContext} from '../../../utils/context'
+import { useRouter } from 'next/navigation'
 
 export default function page() {
-  return (
-    <div>
-        <iframe src='https://giacaphe.com/gia-ca-phe-truc-tuyen/' className='w-full h-[92.2vh]'/>
-    </div>
-  )
+  const {setIsLoading} = useAppContext()
+  const router = useRouter()
+  useEffect(()=> {
+    const isLogin = localStorage.getItem('login')
+    if(isLogin) return setIsLoading(false)
+    setIsLoading(true)
+    router.push('/login')
+  }, [])
+    return (
+        <div>
+            <iframe
+                src="https://giacaphe.com/gia-ca-phe-truc-tuyen/"
+                className="w-full h-[92.2vh]"
+            />
+        </div>
+    )
 }
--- app/main/calculator/page.tsx
+++ app/main/calculator/page.tsx
@@ -1,9 +1,19 @@
 'use client'
 
-import React from 'react'
+import {useEffect} from 'react'
 import { useAppContext } from "../../../utils/context";
+import {useRouter} from 'next/navigation'
 
 export default function calculatorPage() {
+  const {setIsLoading} = useAppContext()
+  const router = useRouter()
+  useEffect(()=> {
+    const isLogin = localStorage.getItem('login')
+    if(isLogin) return setIsLoading(false)
+    setIsLoading(true)
+    router.push('/login')
+  }, [])
+
   const {isDark} = useAppContext()
   return (
     <div className={isDark ? 'dark': ''}>page</div>
